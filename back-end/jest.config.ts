a8743ed629b25971d8059eff14d2ed34eb2bb98c
Pablo Souza <pgeovany14@gmail.com>
2022-09-22 22:48:29 -0300

    chore: set collectCoverage to true and add an array with paths to ignore

--- back-end/jest.config.ts
+++ back-end/jest.config.ts
@@ -3,6 +3,18 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'repositories',
+    'jestGlobalMocks.ts',
+    '<rootDir>/src/server.ts',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/config',
+    '<rootDir>/tests/factories',
+  ],
 };
 
 export default config;
