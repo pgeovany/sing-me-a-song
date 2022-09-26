7a9d6c39a54ea74f030ee867af5e1e94445eac7e
Pablo Souza <pgeovany14@gmail.com>
2022-09-26 16:34:08 -0300

    test: implement e2e test for creating a recommendation

--- front-end/cypress/e2e/home.cy.js
+++ front-end/cypress/e2e/home.cy.js
@@ -4,16 +4,26 @@ beforeEach(() => {
 
 describe('Home screen', () => {
   it('Should load the recommendations', () => {
-    cy.seedDatabase();
+    cy.seedDatabase(15);
     cy.visit('http://localhost:3000/');
   });
 
   it('Should create a recommendation', () => {
     cy.visit('http://localhost:3000/');
+
     cy.get('[data-cy="name"]').type('Bob Dylan - Masters of War');
     cy.get('[data-cy="url"]').type(
       'https://www.youtube.com/watch?v=JEmI_FT4YHU'
     );
+
+    cy.intercept('POST', '/recommendations').as('createRecommendation');
+
     cy.get('[data-cy="submit"]').click();
+
+    cy.wait('@createRecommendation').should(({ response }) => {
+      const { statusCode } = response;
+
+      expect(statusCode).to.eq(201);
+    });
   });
 });
