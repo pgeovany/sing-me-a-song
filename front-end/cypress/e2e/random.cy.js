479ec9c590a21991da0b9829ab20dba58fea07c8
Pablo Souza <pgeovany14@gmail.com>
2022-09-26 16:46:54 -0300

    test: adjust seedDatabase command call to include the now mandatory param

--- front-end/cypress/e2e/random.cy.js
+++ front-end/cypress/e2e/random.cy.js
@@ -3,8 +3,8 @@ beforeEach(() => {
 });
 
 describe('Random recommendations', () => {
-  it('Should load a random recommendations', () => {
-    cy.seedDatabase();
+  it('Should load a random recommendation', () => {
+    cy.seedDatabase(10);
     cy.visit('http://localhost:3000/random');
   });
 });
--- front-end/cypress/e2e/top.cy.js
+++ front-end/cypress/e2e/top.cy.js
@@ -4,7 +4,7 @@ beforeEach(() => {
 
 describe('Top recommendations', () => {
   it('Should load the recommendations ordered from highest to lowest score', () => {
-    cy.seedDatabase();
+    cy.seedDatabase(10);
     cy.visit('http://localhost:3000/top');
   });
 });
